e9f0d020b51198e72d24605edee4831088d56a13
Putraokta <oputra830@gmail.com>
2025-03-14 10:10:19 +0700

    first init github

---
+++ 02_Pengenalan_IDE_dan_Pemrograman_NodeJs/variables.js
@@ -0,0 +1,14 @@
+let name = "Putra Pratama Okta Riano";
+const nim = "2211104068";
+
+console.log(name);
+console.log("Nama saya adalah " + name + " dan NIM saya adalah " + nim);
+console.log(`Nama saya adalah ${name} dan NIM saya adalah ${nim}`);
+
+console.log(`Nama saya adalah ${name}
+NIM saya adalah ${nim}`);
+
+name = "ayam"; // Mengubah nilai name, diperbolehkan karena menggunakan let
+
+let fullName = "Putra Pratama Okta Riano"; // Jika tidak digunakan, sebaiknya dihapus atau dipakai
+console.log(fullName);
