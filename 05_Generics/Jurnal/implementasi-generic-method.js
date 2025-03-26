0d60139d85aa6b84f957900985fe6d9759f733d3
Putraokta <oputra830@gmail.com>
2025-03-26 13:46:15 +0700

    menambahkan class Penjumlahan dengan method JumlahTigaAngka

---
+++ 05_Generics/Jurnal/implementasi-generic-method.js
@@ -0,0 +1,8 @@
+class Penjumlahan {
+    penjumlahanTigaAngka(a, b, c) {
+        let hasil = a + b + c;
+        return hasil;
+    }
+}
+
+module.exports = Penjumlahan;  // Menambahkan export agar bisa dipakai di file lain
