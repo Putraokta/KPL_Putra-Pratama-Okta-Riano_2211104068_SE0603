ac7431d74d12bc1b1605b2f99501a6a7d019e32d
Putraokta <oputra830@gmail.com>
2025-03-30 07:46:48 +0700

    Menambahkan class SayaTubeVideo

--- 06_Design_by_Contract_dan_Defensive_Programming/TP/SayaTubeVideo.js
+++ 06_Design_by_Contract_dan_Defensive_Programming/TP/SayaTubeVideo.js
@@ -1,7 +1,7 @@
 class SayaTubeVideo {
     constructor(title) {
-      if (!title) {
-        throw new Error("Judul video harus diisi.");
+      if (!title || title.length > 100) {
+        throw new Error("Judul video tidak valid.");
       }
       this.id = Math.floor(10000 + Math.random() * 90000);
       this.title = title;
@@ -9,14 +9,23 @@ class SayaTubeVideo {
     }
   
     IncreasePlayCount(count) {
-      if (count <= 0) {
-        throw new Error("Jumlah putar harus lebih dari 0.");
+      if (count <= 0 || count > 10000000) {
+        throw new Error("Input play count tidak valid.");
       }
-      this.playCount += count;
+      try {
+        if (this.playCount + count > Number.MAX_SAFE_INTEGER) {
+          throw new Error("Overflow play count.");
+        }
+        this.playCount += count;
+      } catch (error) {
+        console.error("Terjadi exception:", error.message);
+      }
     }
   
     PrintVideoDetails() {
-      return `ID: ${this.id}, Judul: ${this.title}, Jumlah Putar: ${this.playCount}`;
+      console.log(`ID: ${this.id}`);
+      console.log(`Judul: ${this.title}`);
+      console.log(`Jumlah Putar: ${this.playCount}`);
     }
   }
   
